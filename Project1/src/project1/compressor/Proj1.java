166d132d483c7826759c25241d327829dec2761b
Arturo Salinas <jasalina@ncsu.edu>
2018-06-05 23:24:18 -0400

    Added starter code for linkedlist

--- Project1/src/project1/compressor/Proj1.java
+++ Project1/src/project1/compressor/Proj1.java
@@ -5,7 +5,7 @@ package project1.compressor;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.AbstractSequentialList;
+import java.util.AbstractList;
 import java.util.Scanner;
 
 /**
@@ -50,6 +50,7 @@ public class Proj1 {
         System.out.println("The file to compress is: " + filename);
         try {
             fileReader = new Scanner(new FileInputStream(filename));
+            LinkedMTFList<String> words = new LinkedMTFList<String>();
             while(fileReader.hasNextLine()) {
                 
                 
@@ -61,6 +62,200 @@ public class Proj1 {
         }
         
         
+        
+    }
+
+}
+
+
+
+
+
+
+/**
+ * The LinkedAbstractList Class.
+ * 
+ * @author Arturo Salinas
+ * @param <E> the element type
+ */
+class LinkedMTFList<E> extends AbstractList<E> {
+
+    /** The front of the list. */
+    private ListNode front;
+
+    /** The size. */
+    private int size;
+
+
+    /**
+     * Instantiates a new linked abstract list.
+     *
+     * @param capacity the maximum capacity of the list
+     */
+    public LinkedMTFList() {
+        front = null;
+        size = 0;
+
+    }
+
+    /**
+     * Gets the list node.
+     *
+     * @param idx the index to get from
+     * @return the list node at the index
+     */
+    private ListNode getListNode(int idx) {
+        ListNode current = front;
+        for (int i = 0; i < idx && current != null; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+    
+
+    /**
+     * Adds the list node
+     * @param idx the index to add at
+     * @param element the element to add
+     */
+    @Override
+    public void add(int idx, E element) {
+        if (element == null) {
+            throw new NullPointerException("Cannot add a null element");
+        }
+        if (idx < 0 || idx > this.size()) {
+            throw new IndexOutOfBoundsException("Problem with specified index");
+        }
+        if (checkDuplicate(element)) {
+            throw new IllegalArgumentException("Cannot have duplicates");
+        }
+        ListNode node = new ListNode(element);
+        ListNode current = front; // should be after the new ListNode
+        ListNode last = null; // should be before the new ListNode
+        for (int i = 0; i < idx && current != null; i++) {
+            last = current;
+            current = current.next;
+        }
+        if (last != null) {
+            last.next = node;
+        } else {
+            front = node;
+        }
+        node.next = current;
+        size++;
+    }
+    
+    /**
+     * Checks if value is already in the list
+     * @param value the value to compare with
+     * @return true if duplicate
+     */
+    public boolean checkDuplicate(E value) {
+        for (ListNode e = front; e != null; e = e.next) {
+            if (e.data.equals(value)) {
+            return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Remove from the list
+     * @param idx the index to remove from
+     */
+    @Override
+    public E remove(int idx) {
+        if (idx < 0 || idx >= size()) {
+            throw new IndexOutOfBoundsException("Problem with specified index");
+        }
+        ListNode old = getListNode(idx);
+        getListNode(idx - 1).next = getListNode(idx + 1);
+        size--;
+        return old.data;
+    }
+
+    /**
+     * Gets from the list.
+     * @param idx the index to get from
+     */
+    @Override
+    public E get(int idx) {
+        if (idx < 0 || idx >= size()) {
+            throw new IndexOutOfBoundsException("Problem with specified index");
+        }
+        ListNode current = front;
+        for (int i = 0; i < idx && current != null; i++) {
+            current = current.next;
+        }
+        if (current != null) {
+            return current.data;
+        }
+        return null;
+    }
+
+    /**
+     * Sets the value of an element in the list
+     * @param idx the index to set at
+     * @param element the value to set
+     */
+    @Override
+    public E set(int idx, E element) {
+        if (element == null) {
+            throw new NullPointerException("Cannot set a null element");
+        }
+        if (idx < 0 || idx >= size()) {
+            throw new IndexOutOfBoundsException("Problem with specified index");
+        }
+        ListNode node = getListNode(idx);
+        if (node != null) {
+            ListNode newNode = new ListNode(element);
+            getListNode(idx - 1).next = newNode;
+            newNode.next = node.next;
+            return node.data;
+        }
+        return null;
+    }
+
+    /**
+     * Gets the size of the list
+     */
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * The ListNode inner class.
+     */
+    private class ListNode {
+
+        /** The data. */
+        private E data;
+    
+        /** The next. */
+        private ListNode next;
+    
+        /**
+         * Instantiates a new list node.
+         *
+         * @param data the value to store
+         */
+        public ListNode(E data){
+            this.data = data;
+        }
+    
+        
+        /**
+         * Instantiates a new list node.
+         *
+         * @param data the value to store
+         * @param next the next ListNode
+         *//*
+        public ListNode(E data, ListNode next) {
+            this.data = data;
+            this.next = next;
+        }
+        */
         
     }
 
